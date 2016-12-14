58d087125856e0b011c698227b714645d3f2b2d1
zhykovegor <zhykovegor@gmail.com>
2016-12-13 16:56:56 -0800

    updated manifest and added retrofit

--- app/src/main/java/com/translator/translator/MainActivity.java
+++ app/src/main/java/com/translator/translator/MainActivity.java
@@ -2,7 +2,6 @@ package com.translator.translator;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,34 +11,159 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import entry.TranslatedData;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import services.Url;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private TextView text;
+    private TextView translated;
 
+    private String selectedLanguage;
+    private Gson gson = new GsonBuilder().create();
+    private final String BASE_URL = "https://translate.yandex.net";
+    private final String KEY = "trnsl.1.1.20161213T090001Z.b58c535961cc82b7.181d722d1506c2ac62e75282e49c101705e43a2f";
+
+    //setting retrofit
+    private Retrofit retrofit = new Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(BASE_URL)
+            .build();
+
+    //setting service
+    private Url service = retrofit.create(Url.class);
+
+
+    //Finding views
+    private void findViews() {
+        text = (TextView) findViewById(R.id.text);
+        translated = (TextView) findViewById(R.id.translated);
+    }
+
+    //Toolbar
+    private Toolbar getToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        return toolbar;
+    }
+
+    //FloatingButton
+    private void initFloatingButton() {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Map<String, String> params = new HashMap<>();
+                params.put("key", KEY);
+                params.put("text", text.getText().toString());
+                params.put("lang", selectedLanguage);
+
+                Call<TranslatedData> call = service.update(params);
+
+                call.enqueue(new Callback<TranslatedData>() {
+                    @Override
+                    public void onResponse(Call<TranslatedData> call, Response<TranslatedData> response) {
+                        //cutting not useful symbols
+                        String currentText = response.body().getText().toString();
+                        currentText = currentText.substring(1, currentText.length() - 1);
+
+                        //setting test from respond to text field
+                        translated.setText(currentText);
+                    }
+
+                    @Override
+                    public void onFailure(Call<TranslatedData> call, Throwable t) {
+                        //fail message
+                        Toast.makeText(getBaseContext(), "Error! Check internet connection!", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
+    }
 
+    //NavigationView
+    private void initNavigationView() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    //Drawer
+    private void initDrawer(Toolbar toolbar) {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+    }
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+    //Spinner
+    private void initSpinner() {
+        //array contains allowed languages
+        final String[] languages = {"en-ru", "ru-en"};
+        //adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //spinner
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+
+        spinner.setSelection(0);
+
+        //event listener
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedLanguage = languages[i];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //init toolbar
+        Toolbar toolbar = getToolbar();
+        setSupportActionBar(toolbar);
+
+        //init floating button
+        initFloatingButton();
+
+        //init navigation view
+        initNavigationView();
+
+        //init drawer
+        initDrawer(toolbar);
+
+        //init spinner
+        initSpinner();
+
+        //finding views
+        findViews();
+
     }
 
     @Override
---
+++ app/src/main/java/entry/TranslatedData.java
@@ -0,0 +1,33 @@
+package entry;
+
+import java.util.ArrayList;
+
+public class TranslatedData {
+    private int code;
+    private String lang;
+    private ArrayList<String> text;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public ArrayList<String> getText() {
+        return text;
+    }
+
+    public void setText(ArrayList<String> text) {
+        this.text = text;
+    }
+}
